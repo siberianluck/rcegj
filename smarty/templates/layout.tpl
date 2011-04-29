c20b98c17eae27eb14411ae497e60c7cdf3329d0
Ben Mason <bomason@gmail.com>
2011-04-28 18:32:47 -0600

    layout boxes

--- smarty/templates/layout.tpl
+++ smarty/templates/layout.tpl
@@ -15,25 +15,31 @@
     </head>
     <body>
         <div class="container">
-			<div id="logo" class="span-6">
-				<img src="img/logo.png" />
-			</div>
-			<div id="companyName" class="span-12">
-				<span>Rolland Consulting Engineers</span>
-			</div>
-			<div id="headerImg" class="span-6 last">
-				<img src="img/road.jpg" />
-			</div>
-			<div id="menu" class="span-6">	
-				<ul>
-					<li>About</li>
-					<li>Staff</li>
-					<li>Projects</li>
-				</ul>
-			</div>
-			<div id="content" class="span-18 last">
-				{block name=content}{/block}
-			</div>
+            <div id="logo" class="span-6">
+                    <img src="img/logo.png" />
+            </div>
+            <div id="companyName" class="span-12">
+                    <span>Rolland Consulting Engineers</span>
+            </div>
+            <div id="headerImg" class="span-6 last">
+                    <img src="img/road.jpg" />
+            </div>
+            <div class="span-6">
+                <div id="menu" class="span-6 last">	
+                        <ul>
+                                <li>About</li>
+                                <li>Staff</li>
+                                <li>Projects</li>
+                        </ul>
+                </div>
+                <div id="colorBlock" class="span-6 last"></div>
+                <div id="sidebarImg" class="span-6 last">
+                    <img src="img/bridge.jpg" />
+                </div>
+            </div>
+            <div id="content" class="span-18 last">
+                    {block name=content}{/block}
+            </div>
         </div>
     </body>
 </html>
--- smarty/templates_c/0c04115701025fdf186966beead194600e6e675d.file.index.tpl.php
+++ smarty/templates_c/0c04115701025fdf186966beead194600e6e675d.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.0.7, created on 2011-04-23 10:49:23
+<?php /* Smarty version Smarty-3.0.7, created on 2011-04-28 17:37:24
          compiled from "../smarty/templates/index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:16181790304db3031307d7c1-95686427%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
@@ -7,13 +7,13 @@ $_smarty_tpl->decodeProperties(array (
     '0c04115701025fdf186966beead194600e6e675d' => 
     array (
       0 => '../smarty/templates/index.tpl',
-      1 => 1303577351,
+      1 => 1304032462,
       2 => 'file',
     ),
     'e79478cf52ba14fea344c76f0d87e53c7096b9a6' => 
     array (
       0 => '../smarty/templates/layout.tpl',
-      1 => 1303577206,
+      1 => 1304033841,
       2 => 'file',
     ),
   ),
@@ -28,26 +28,49 @@ $_smarty_tpl->decodeProperties(array (
     <head>
         <title>Rolland Consulting Engineers - <?php echo $_smarty_tpl->getVariable('page')->value;?>
 </title>
+		
+		<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print"> 
+		<!--[if lt IE 8]>
+		<link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
+		<![endif]-->
+		
+		<!-- Page styles --> 
+		<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" /> 
+	
     </head>
     <body>
-        <div id="header">
-            <span>Rolland Consulting Engineers</span>
-        </div>
-        <div id="menu">
-            <ul>
-                <li>About</li>
-            </ul>
-        </div>
-        <div id="content">
-        
-<H3>About Us</h3>
-<p>Rolland Consulting Engineers is clearly the best company for any engineering
-job you can imagine.  They have legendary surveyors and civil engineers eager 
-to do your every bidding</p>
+        <div class="container">
+            <div id="logo" class="span-6">
+                    <img src="img/logo.png" />
+            </div>
+            <div id="companyName" class="span-12">
+                    <span>Rolland Consulting Engineers</span>
+            </div>
+            <div id="headerImg" class="span-6 last">
+                    <img src="img/road.jpg" />
+            </div>
+            <div class="span-6">
+                <div id="menu" class="span-6 last">	
+                        <ul>
+                                <li>About</li>
+                                <li>Staff</li>
+                                <li>Projects</li>
+                        </ul>
+                </div>
+                <div id="colorBlock" class="span-6 last"></div>
+                <div id="sidebarImg" class="span-6 last">
+                    <img src="img/bridge.jpg" />
+                </div>
+            </div>
+            <div id="content" class="span-18 last">
+                    
+	<H3>About Us</h3>
+	<p>Rolland Consulting Engineers is clearly the best company for any engineering
+	job you can imagine.  They have legendary surveyors and civil engineers eager 
+	to do your every bidding</p>
 
-        </div>
-        <div id="footer">
-            <span>Created by Ben Mason</span>
+            </div>
         </div>
     </body>
 </html>
