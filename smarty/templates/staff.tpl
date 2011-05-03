ee9902bca9e2fa9cd2aa5bd37a657caea469d91d
Ben Mason <bomason@gmail.com>
2011-05-03 10:04:30 -0600

    style staff page

--- smarty/templates/staff.tpl
+++ smarty/templates/staff.tpl
@@ -1,7 +1,7 @@
 {extends file="layout.tpl"}
 {block name=content}
 {foreach $emps as $emp}
-    <div class="span-8 {cycle name="empBlocks" values="append-1,last"}">
+    <div class="span-8 {cycle name="empBlocks" values="append-2,last"}">
         <div class="span-4">
             <div class="imgCont">
                 <img src="/img/{$emp.photo}" />
--- www/staff.php
+++ www/staff.php
@@ -8,19 +8,24 @@ $smarty->setCompileDir('../smarty/templates_c');
 $smarty->setCacheDir('../smarty/cache');
 $smarty->setConfigDir('../smarty/configs');
 
+/******************************************************************************
+* Staff Array
+* Put names of staff in here along with bios and photos.
+* Photos should be 149px wide by 177px high
+*****************************************************************************/
 $staff = array(
             array(
-                "name" => "Eric Slivon",
+                "name" => "Eric<br />Slivon",
                 "bio" => "Having travelled the world, Eric has experienced and come to understand many exotic cultures.  He applies this broad understanding of the world to his trade, enabling him to 'think out of the box' and produce surprising and creative solutions to any problem.",
                 "photo" => "eric.jpg"
             ),
             array(
-                "name" => "Kent Shaffer",
+                "name" => "Kent<br />Shaffer",
                 "bio" => "A dedicated engineer and family man, Kent is satisfied with nothing less than perfection.  Armed with the experience granted by the completion of hundreds of projects, Kent delivers stunning, rock solid results.",
                 "photo" => "kent.jpg"
             ),
             array(
-                "name" => "Rick Mason",
+                "name" => "Rick<br />Mason",
                 "bio" => "This legendary western slope surveyor has served the region for over 30 years.  He takes immense pride in his craft and even helped found the WCLS.  If you haven't already had Rick survey your land, your neighbor probably has.",
                 "photo" => "rick.jpg"
             ),
